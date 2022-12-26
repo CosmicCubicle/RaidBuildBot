581194310285c2b8bce188ba172336f59d504a03
CosmicCubicle <admin@cosmiccubicle.com>
2022-12-26 13:23:48 +0000

    Update Modules

--- commands/raidBuild.js
+++ commands/raidBuild.js
@@ -70,8 +70,10 @@ module.exports = {
 					if (data.Strategy[ind] == null ){
 						data.Strategy[ind] = "N/A"
 					};
-					
-					var response = response + `This Pokemon is rated in Tier: ${data.Grade[ind]}\n\nYour ${data.Role[ind]} ${data.BuildType[ind]} Build is: \n\n${data.Pokemon[ind]} @ ${data.HeldItem[ind]}\nAbility: ${data.Ability[ind]}\nLevel: 100\nTera Type: ${data.TeraType[ind]}\nEVs: ${(data.EVs[ind].split('\n'))[0]} / ${(data.EVs[ind].split('\n'))[1]} / ${(data.EVs[ind].split('\n'))[2]}\n${data.Nature[ind]} Nature\n- ${data.MoveSets[ind].split('\n')[0]}\n- ${data.MoveSets[ind].split('\n')[1]}\n- ${data.MoveSets[ind].split('\n')[2]}\n- ${data.MoveSets[ind].split('\n')[3]}\n\nYou can ignore the Following IVs when training this Pokemon: ${data.IgnoreIv[ind]}\n\nStrategy and Build Notes:\n${data.Strategy[ind]}\n\n ---------------\n\n`;
+					if (data.IgnoreIv[ind] == null ){
+						data.Strategy[ind] = "N/A"
+					};
+					var response = response + `This Pokemon is rated in Tier: ${data.Grade[ind]}\n\nYour ${data.Role[ind]} ${data.BuildType[ind]} Build is: \n\n${data.Pokemon[ind]} @ ${data.HeldItem[ind]}\nAbility: ${data.Ability[ind]}\nLevel: 100\nTera Type: ${data.TeraType[ind]}\nEVs: ${(data.EVs[ind].split('\n'))[0]} / ${(data.EVs[ind].split('\n'))[1]} / ${(data.EVs[ind].split('\n'))[2]}\n${data.Nature[ind]} Nature\n- ${data.MoveSets[ind].split('\n')[0]}\n- ${data.MoveSets[ind].split('\n')[1]}\n- ${data.MoveSets[ind].split('\n')[2]}\n- ${data.MoveSets[ind].split('\n')[3]}\n\nYou can ignore the Following IVs when training this Pokemon:\n${data.IgnoreIv[ind]}\n\nStrategy and Build Notes:\n${data.Strategy[ind]}\n -----------------------------------------------\n`;
 				}
 				return interaction.reply(response)
 			}
