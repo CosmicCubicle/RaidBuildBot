038beabf5107a132b6687fe124af861963b858d3
CosmicCubicle <admin@cosmiccubicle.com>
2023-01-01 03:48:59 +0000

    Update EventBuilds Commands

--- commands/eventBuild.js
+++ commands/eventBuild.js
@@ -50,12 +50,11 @@ module.exports = {
 				response = 'No Results found for ' + interaction.options.getString('pokemon') + '. Please make sure you have spelled the name of the pokemon correctly. You can manually check the sheet by visiting:  https://docs.google.com/spreadsheets/d/1bMaLkMyy4cs7O5RXLNMS95xPtVz-FVhUB_Q-4B9a-UE/edit?usp=sharing'
 				
 				if (datafind.indexes.length == 0){
-					return interaction.reply();
 				}
 				else
 				{
+					response = "";
 					for (var i = 0; i < datafind.indexes.length; i++){
-						response = "";
 						var ind = datafind.indexes[i];
 						if (interaction.options.getString('event').toLowerCase() == builddata.EventBuild[ind].toLowerCase()){
 							if (builddata.Strategy[ind] == null ){
@@ -66,9 +65,9 @@ module.exports = {
 							var response = response + `This Pokemon is rated in Tier: ${builddata.Grade[ind]}\n\nYour ${builddata.Role[ind]} ${builddata.BuildType[ind]} Build is: \n\n${builddata.Pokemon[ind]} @ ${builddata.HeldItem[ind]}\nAbility: ${builddata.Ability[ind]}\nLevel: 100\nTera Type: ${builddata.TeraType[ind]}\nEVs: ${(builddata.EVs[ind].split('\n'))[0]} / ${(builddata.EVs[ind].split('\n'))[1]} / ${(builddata.EVs[ind].split('\n'))[2]}\n${builddata.Nature[ind]} Nature\n- ${builddata.MoveSets[ind].split('\n')[0]}\n- ${builddata.MoveSets[ind].split('\n')[1]}\n- ${builddata.MoveSets[ind].split('\n')[2]}\n- ${builddata.MoveSets[ind].split('\n')[3]}\n\nYou can ignore the Following IVs when training this Pokemon:\n${IgnoreIvs}\n\nStrategy and Build Notes:\n${builddata.Strategy[ind]}\n -----------------------------------------------\n`;
 						}
 						
-						return interaction.reply(response);
 					}
 				}
+				return interaction.reply(response);
 			}
 		}
 	}
