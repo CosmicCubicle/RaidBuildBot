0583b699681a4aa5724653d2a837de19c11eeea6
CosmicCubicle <admin@cosmiccubicle.com>
2023-09-07 03:47:11 +0000

    CHanged some things

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setDescription('Get Help'),
 	async execute(interaction) {
 		
-		var response = 'Using the Tera Raid Builds Bot:\n\nThe Bot consists of 2 commands:\n/help\n/raidbuild\n\nEXAMPLE:\n/raidbuild pokemon:Charizard\n\nResponse:\nThis Pokemon is rated in Tier: B\n\nYour SOLO/TEAM DAMAGE PHYSICAL Build is:\n\nCharizard @ Sitrus Berry\n\nAbility: Blaze\nLevel: 100\nTera Type: Flying\nEVs: 252 HP / 252 Atk / 4 Def\nAdamant Nature\n- Belly Drum\n- Acrobatics\n- Fire Punch\n- Outrage\n\nYou can ignore the Following IVs when training this Pokemon:\nSpeed, Special Attack\n\nStrategy and Build Notes:\nNeeds Support for Cinderace Event\n-----------------------------------------------\n\n\n\nFor additional support please contact on Discord at \nhttps://discord.gg/vJyUfDnR\n\nOr via email at\ncontact@cosmiccubicle.com\n'
+		var response = 'Using the Tera Raid Builds Bot:\n\nThe Bot consists of 2 commands:\n/help\n/raidbuild\n\nEXAMPLE:\n/raidbuild pokemon:Charizard\n\nResponse:\nThis Pokemon is rated in Tier: B\n\nYour SOLO/TEAM DAMAGE PHYSICAL Build is:\n\nCharizard @ Sitrus Berry\n\nAbility: Blaze\nLevel: 100\nTera Type: Flying\nEVs: 252 HP / 252 Atk / 4 Def\nAdamant Nature\n- Belly Drum\n- Acrobatics\n- Fire Punch\n- Outrage\n\nYou can ignore the Following IVs when training this Pokemon:\nSpeed, Special Attack\n\nStrategy and Build Notes:\nNeeds Support for Cinderace Event\n-----------------------------------------------\n\n\n\nFor additional support please contact on Discord at \nhttps://discord.gg/eeQsc6FyqS\n\nOr via email at\ncontact@cosmiccubicle.com\n'
 		return interaction.reply(response)
 
 		}
--- events/ready.js
+++ events/ready.js
@@ -1,16 +1,18 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
 	name: 'ready',
 	once: true,
 
 	execute(client) {
+		const guilds = client.guilds.cache;
 		const guild = client.guilds.cache.get('1055920563178188820');
 		const channel = guild.channels.cache.get('1068936939878752257');
 		var message = '';
 
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 		channel.send(`Ready! Logged in as ${client.user.tag}`);
-		
+		channel.send(`Test ${guilds.size}`);
+		client.user.setActivity(`${guilds.size} Servers Raid`, {type: ActivityType.Watching});		
 	},
 };
